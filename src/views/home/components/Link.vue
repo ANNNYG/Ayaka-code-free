92d0a3a4f9828060907bdb92606b5d1135b18bd4
Zyg <646124200@qq.com>
2023-10-17 15:23:17 +0800

    fix: 🐛  链接样式调整

--- src/views/home/components/Link.vue
+++ src/views/home/components/Link.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="canvas">
     <RouterLink :to="link" class="link">{{ text }}</RouterLink>
+    <Handle :position="Position.Top" />
   </div>
 </template>
 
 <script setup lang="ts">
+import { Handle, Position } from '@vue-flow/core'
 import type { RouteLocationRaw } from 'vue-router'
 
 type PROPS_TYPE = {
